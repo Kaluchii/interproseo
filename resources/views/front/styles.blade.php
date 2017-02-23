ba5637d9a27aaab227d5d6111a41fd4721c956ff
Kaluchii <mr.kaluchii@mail.ru>
2017-02-23 12:10:17 +0600

    Optimize js and media query for mobile.css

--- resources/views/front/styles.blade.php
+++ resources/views/front/styles.blade.php
@@ -1,4 +1,4 @@
 @section('styles')
     <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="/css/mobile.css">
+    <link rel="stylesheet" href="/css/mobile.css" media="max-width:1150px">
 @endsection
